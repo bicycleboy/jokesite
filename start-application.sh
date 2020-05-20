728bef9f4e1fe452308701c96d7e43e395da60b9
Chris <chris@galah.gateway>
2020-05-20 17:04:27 +1000

    startup

--- start-application.sh
+++ start-application.sh
@@ -6,22 +6,28 @@ else
     PORT=$1
 fi
 
-# create database 
-FILE=/var/tmp/jokesiteDatabaseCreated
-if [ -f $FILE ]; then
-    echo "Database already created"
-else
-    python3 "manage.py" "migrate"
-    python3 "manage.py" "loaddata" "jokes.yaml"
-    touch $FILE
-fi
-# Probs not needed, wass for podman + sqllite 
+# Probs not needed, was for podman + sqllite 
 if [ -z ${DATABASE_SERVICE_NAME} ] ; then
     export DJANGO_SETTINGS_MODULE=jokesite.devsettings
-    echo "Using development settings"
-else
+    echo "start-application.sh: Using development settings"
+else 
+    # Could not get init container to work
+    for i in {1..5}; do
+        nslookup ${DATABASE_SERVICE_NAME} | grep "No answer"
+        if [ $? -eq 1 ]; then
+            echo "start-application.sh: Found DB service ${DATABASE_SERVICE_NAME}"
+            break
+        else
+            echo "start-application.sh: Waiting for DB service..."
+            sleep 2
+        fi
+    done
+    echo "start-application.sh: Creating Database"
+    python3 "manage.py" "migrate"
+    echo "start-application.sh: Populating Database"
+    python3 "manage.py" "loaddata" "jokes.yaml"
     export DJANGO_SETTINGS_MODULE=jokesite.settings
-    echo "Using Postgres: ${DATABASE_SERVICE_NAME}"
+    echo "start-application.sh: Using Postgres DB: ${DATABASE_SERVICE_NAME}"
 fi
 
 gunicorn jokesite.wsgi:application --bind 0.0.0.0:${PORT}
